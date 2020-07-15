2373edeb54e187422a6e89a57e3b00833261c759
daijianyong <daijianyong@filemanager.com>
2020-07-15 22:49:42 +0800

    github

--- src/com/example/testandroid/MainActivity.java
+++ src/com/example/testandroid/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main1);
+		setContentView(R.layout.activity_main);
 	}
 
 }
